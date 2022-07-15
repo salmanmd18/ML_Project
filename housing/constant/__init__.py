7b341380e702d7b0bfee7c81b3c5f1398acd9714
Mohammed Salman <salmanmohammed2018@gmail.com>
2022-07-15 20:04:36 +0530

    Completed

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -2,6 +2,11 @@
 
 import os
 from datetime import datetime
+
+def get_current_time_stamp():
+    return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
+
 ROOT_DIR = os.getcwd()  #to get current working directory
 CONFIG_DIR = "config"
 CONFIG_FILE_NAME = "config.yaml"
